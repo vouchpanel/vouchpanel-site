06cfe8682b0e4b1b9c706d1fa5d835ca65398552
Carter Sprigings <carter.sprigings@gmail.com>
2021-04-07 13:55:32 -0400

    Check if wall still works.

--- src/pages/OurWall.tsx
+++ src/pages/OurWall.tsx
@@ -1,10 +1,12 @@
 import { Header } from "../components/Header";
 import React from "react";
+import Wall from "@vouchpanel/react";
 
 const OurWall = (props: {}) => {
   return (
     <div>
       <Header></Header>
+      <Wall id={"4"} darkMode="off"></Wall>
     </div>
   );
 };
